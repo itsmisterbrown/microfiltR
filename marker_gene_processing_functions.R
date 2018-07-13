7b667263949979ddef7d1c1cbb2c42519b422d50
itsmisterbrown <itsmisterbrown@gmail.com>
2018-07-13 13:11:41 -0700

    Update marker_gene_processing_functions.R
    added processing scripts into filter.threshold and estimate.ASthreshold. code is generally much cleaner

--- marker_gene_processing_functions.R
+++ marker_gene_processing_functions.R
@@ -1,6 +1,17 @@
 ##functions for processing compositional microbiome data
 #all scripts written by BPB, 071318
 
+#ERROR PROTECTION FUNCTIONS
+format.ASV.tab <- function(ps){
+  if(as.logical(class(phyloseq::otu_table(ps))[1] == "otu_table") && 
+     as.logical(taxa_are_rows(phyloseq::otu_table(ps)) == TRUE)){
+    asv.tab <- as.matrix(phyloseq::otu_table(ps))
+  } else {
+    asv.tab <- as.matrix(t(phyloseq::otu_table(ps)))
+  }
+  asv.tab
+}
+
 #PROCESSING FUNCTIONS
 getCV <- function(ps, WSF=NULL, CVmin, CVmax, CVstep){
   
@@ -97,13 +108,7 @@ getPrev <- function(ps, WSF=NULL, Pmin, Pmax, Pstep){
   
   ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
   
-  #ASV table error protection
-  if(as.logical(class(phyloseq::otu_table(ps.ws))[1] == "otu_table") && 
-     as.logical(taxa_are_rows(phyloseq::otu_table(ps.ws)) == TRUE)){
-    otu.tab <- as.matrix(phyloseq::otu_table(ps.ws))
-  } else {
-    otu.tab <- as.matrix(t(phyloseq::otu_table(ps.ws)))
-  }
+  asv.tab <- format.ASV.tab(ps.ws)
   
   #build param vectors
   l.p <- seq(from = Pmin, to = Pmax, by = Pstep)
@@ -112,7 +117,7 @@ getPrev <- function(ps, WSF=NULL, Pmin, Pmax, Pstep){
   taxa.cvec <- c()
   
   #get prevalence list for each taxon
-  taxa.plist <- apply(X = otu.tab, MARGIN = 1, FUN = function(x){names(x)[which(x!=0)]})
+  taxa.plist <- apply(X = asv.tab, MARGIN = 1, FUN = function(x){names(x)[which(x!=0)]})
   #populate vector of sample counts per ASV
   for(j in 1:length(taxa.plist)){
     taxa.cvec[j] <- length(taxa.plist[[j]])
@@ -150,6 +155,81 @@ getPrev <- function(ps, WSF=NULL, Pmin, Pmax, Pstep){
   
 }
 
+CVfilter <- function(ps, WSF=NULL, CVF){
+  
+  if(is.null(WSF)){
+    message('Warning: WS filtering is highly recommended to reduce cross contamination')
+  }
+  
+  #perform WS filtering
+  filterfx = function(x){
+    x[(x / sum(x)) < WSF] <- 0
+    return(x)
+  }
+  
+  ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+  
+  #perform filter
+  ps.ws <- phyloseq::filter_taxa(ps.ws, function(x) sd(x)/mean(x) > CVF, TRUE)
+  ps.ws
+  
+}
+
+
+RAfilter<- function(ps, WSF=NULL, RAF){
+  
+  if(is.null(WSF)){
+    message('Warning: WS filtering is highly recommended to reduce cross contamination')
+    }
+  
+  #perform WS filtering
+  filterfx = function(x){
+    x[(x / sum(x)) < WSF] <- 0
+    return(x)
+  }
+  
+  ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+  
+  #perform filter
+  raf <- sum(phyloseq::taxa_sums(ps.ws)) * RAF
+  ps.ws <- phyloseq::prune_taxa(taxa_sums(ps.ws)>=raf, ps.ws)
+  ps.ws
+
+}
+
+Pfilter <- function(ps, WSF=NULL, PF){
+  
+  if(is.null(WSF)){
+    message('Warning: WS filtering is highly recommended to reduce cross contamination')
+  }
+  
+  #perform WS filtering
+  filterfx = function(x){
+    x[(x / sum(x)) < WSF] <- 0
+    return(x)
+  }
+  
+  ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+  
+  #format asv table
+  asv.tab <- format.ASV.tab(ps.ws)
+  
+  #get prevalence list for each taxon
+  taxa.plist <- apply(X = asv.tab, MARGIN = 1, FUN = function(x){names(x)[which(x!=0)]})
+  #populate vector of sample counts per ASV
+  for(j in 1:length(taxa.plist)){
+    taxa.cvec[j] <- length(taxa.plist[[j]])
+  }
+  names(taxa.cvec) <- names(taxa.plist)
+  prev.count <- phyloseq::nsamples(ps.ws)* PF
+  taxa.cvec.f <- taxa.cvec[which(taxa.cvec > prev.count)]
+  tn.cvec.f <- names(taxa.cvec.f)
+  #perform filter
+  ps.ws <- phyloseq::prune_taxa(tn.cvec.f, ps.ws)
+  ps.ws
+
+}
+
 #WRAPPER FUNCTIONS
 estimate.WSthreshold <- function(ps, WSmin=1e-4, WSmax=2e-4, WSstep=1e-5, controlID) {
   
@@ -168,17 +248,12 @@ estimate.WSthreshold <- function(ps, WSmin=1e-4, WSmax=2e-4, WSstep=1e-5, contro
         return(x)
       }
       
-      ps.if <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+      ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+      asv.tab <- format.ASV.tab(ps.ws)
       
       #FILTERING
-      if(as.logical(class(phyloseq::otu_table(ps.if))[1] == "otu_table") && 
-         as.logical(taxa_are_rows(phyloseq::otu_table(ps.if)) == TRUE)){
-        otu.tab <- as.matrix(phyloseq::otu_table(ps.if))
-      } else {
-        otu.tab <- as.matrix(t(phyloseq::otu_table(ps.if)))
-      }
       
-      tvec[i] <- nrow(otu.tab[which(otu.tab[,match(controlID, colnames(otu.tab))] != 0),])
+      tvec[i] <- nrow(asv.tab[which(asv.tab[,match(controlID, colnames(asv.tab))] != 0),])
       svec[i] <- sum(phyloseq::sample_sums(ps.if))
       pvec[i] <- sum(phyloseq::sample_sums(ps.if))/sum(phyloseq::sample_sums(ps))*100
       
@@ -196,11 +271,19 @@ estimate.WSthreshold <- function(ps, WSmin=1e-4, WSmax=2e-4, WSstep=1e-5, contro
   
 }
 
-estimate.ASthreshold <- function(ps, WSF, Pmin=NULL, Pmax=NULL, Pstep=NULL, CVmin=NULL, 
-                                 CVmax=NULL, CVstep=NULL, RAFmin=NULL, RAFmax=NULL, RAFstep=NULL, independent=FALSE){
+estimate.ASthreshold <- function(ps, WSF, controlID=NULL, controlCAT=NULL, controlFACTOR=NULL, minLIB=NULL, Pmin=NULL, 
+Pmax=NULL, Pstep=NULL, CVmin=NULL, 
+CVmax=NULL, CVstep=NULL, RAFmin=NULL, RAFmax=NULL, RAFstep=NULL, independent=FALSE){
   
   if(is.null(WSF)){
     message('Warning: Estimation of AS filters will not be accurate without first applying WS filter')
+  }
+  
+  #remove samples < minlib
+  if(is.null(minLIB)){
+    ps = ps
+  } else {
+    ps = phyloseq::prune_samples(phyloseq::sample_sums(ps)>=minLIB, ps)
   }
   
   #perform WS filtering
@@ -210,6 +293,18 @@ estimate.ASthreshold <- function(ps, WSF, Pmin=NULL, Pmax=NULL, Pstep=NULL, CVmi
   }
   
   ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
+  
+  #control sample filtering
+  if(any(c(is.null(controlID), is.null(controlCAT), is.null(controlFACTOR)))){
+    message('Warning: if you plan to remove controls during filtering, then it is recommended to remove during estimation')
+  } else {
+  #create sampledf
+  sampledf <- suppressWarnings(as.matrix(phyloseq::sample_data(ps.ws)))
+  #remove controls
+  filtered.names <- rownames(sampledf[which(sampledf[,match(controlCAT, colnames(sampledf))] != controlFACTOR),])
+  sampledf.s <- as.data.frame(sampledf[filtered.names,])
+  phyloseq::sample_data(ps.ws) <- phyloseq::sample_data(sampledf.s)
+  }
   
   if(isTRUE(independent)){
     ps.wsi <- ps.ws
@@ -256,7 +351,8 @@ estimate.ASthreshold <- function(ps, WSF, Pmin=NULL, Pmax=NULL, Pstep=NULL, CVmi
   ts <- taxa_sums(ps.ws)
   tsp <- taxa_sums(ps.ws)/sum(taxa_sums(ps.ws)) * 100
   namevec <- names(ts)
-  cv.asv <- apply(otu.tab[namevec,], MARGIN = 1, FUN = function(x) sd(x)/mean(x))
+  asv.tab <- format.ASV.tab(ps.ws)
+  cv.asv <- apply(asv.tab[namevec,], MARGIN = 1, FUN = function(x) sd(x)/mean(x))
   tax.tab <- phyloseq::tax_table(ps.ws)[namevec,]
   
   #set prev vectors to null if no prev stats desired
@@ -300,29 +396,26 @@ filter.dataset <- function(ps, controlID=NULL, controlCAT=NULL, controlFACTOR=NU
   
   #WS filtering
   if(is.null(WSF)){
+    message('Warning: WS filtering is highly recommended to reduce cross contamination')
     ps.ws <- ps
   } else {
-  filterfx = function(x){
-    x[(x / sum(x)) < WSF] <- 0
-    return(x)
+    filterfx = function(x){
+      x[(x / sum(x)) < WSF] <- 0
+      return(x)
+    }
+    
+    #create WS filtered object
+    ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
   }
   
-  #create WS filtered object
-  ps.ws <- phyloseq::transform_sample_counts(ps, fun = filterfx)
-  }
+  #create sampledf
   sampledf <- suppressWarnings(as.matrix(phyloseq::sample_data(ps.ws)))
   #create per sample filtered sample sum vector
   ifv <- phyloseq::sample_sums(ps.ws)
   #calculate percent filtered, individual
   p.if <- phyloseq::sample_sums(ps.ws)/phyloseq::sample_sums(ps)*100
   
-  #ASV table error protection
-  if(as.logical(class(phyloseq::otu_table(ps.ws))[1] == "otu_table") && 
-     as.logical(taxa_are_rows(phyloseq::otu_table(ps.ws)) == TRUE)){
-    otu.tab <- as.matrix(phyloseq::otu_table(ps.ws))
-  } else {
-    otu.tab <- as.matrix(t(phyloseq::otu_table(ps.ws)))
-  }
+  asv.tab <- format.ASV.tab(ps.ws)
   
   #control sample filtering
   if(any(c(is.null(controlID), is.null(controlCAT), is.null(controlFACTOR)))){
@@ -330,20 +423,20 @@ filter.dataset <- function(ps, controlID=NULL, controlCAT=NULL, controlFACTOR=NU
     tax.tab.subset <- NULL
     ttsn <- NULL
   } else {
-  #control taxa count
-  npos <- nrow(otu.tab[which(otu.tab[,match(controlID, colnames(otu.tab))] != 0),])
-  
-  #taxonomy of taxa in positive control
-  tax.tab <- phyloseq::tax_table(ps.ws)
-  taxanames.control <- rownames(otu.tab[which(otu.tab[,match(controlID, colnames(otu.tab))] != 0),])
-  tax.tab.subset <- tax.tab[taxanames.control] #taxonomy of taxa in positive control
-  ttsn <- tax.tab.subset
-  rownames(ttsn) <- NULL
-  
-  #remove controls
-  filtered.names <- rownames(sampledf[which(sampledf[,match(controlCAT, colnames(sampledf))] != controlFACTOR),])
-  sampledf.s <- as.data.frame(sampledf[filtered.names,])
-  phyloseq::sample_data(ps.ws) <- phyloseq::sample_data(sampledf.s)
+    #control taxa count
+    npos <- nrow(asv.tab[which(asv.tab[,match(controlID, colnames(asv.tab))] != 0),])
+    
+    #taxonomy of taxa in positive control
+    tax.tab <- phyloseq::tax_table(ps.ws)
+    taxanames.control <- rownames(asv.tab[which(asv.tab[,match(controlID, colnames(asv.tab))] != 0),])
+    tax.tab.subset <- tax.tab[taxanames.control] #taxonomy of taxa in positive control
+    ttsn <- tax.tab.subset
+    rownames(ttsn) <- NULL
+    
+    #remove controls
+    filtered.names <- rownames(sampledf[which(sampledf[,match(controlCAT, colnames(sampledf))] != controlFACTOR),])
+    sampledf.s <- as.data.frame(sampledf[filtered.names,])
+    phyloseq::sample_data(ps.ws) <- phyloseq::sample_data(sampledf.s)
   }
   
   #AS filtering
@@ -351,44 +444,21 @@ filter.dataset <- function(ps, controlID=NULL, controlCAT=NULL, controlFACTOR=NU
   if(is.null(RAF)){
     ps.ws <- ps.ws
   } else {
-    raf <- sum(phyloseq::taxa_sums(ps.ws)) * RAF
-    ps.ws <- phyloseq::prune_taxa(taxa_sums(ps.ws)>=raf, ps.ws)
+    ps.ws <- RAfilter(ps = ps.ws, WSF = WSF, RAF = RAF)
   }
   
   #CV filter
   if(is.null(CVF)){
     ps.ws <- ps.ws
   } else {
-    ps.ws <- phyloseq::filter_taxa(ps.ws, function(x) sd(x)/mean(x) > CVF, TRUE)
+    ps.ws <- CVfilter(ps = ps.ws, WSF = WSF, CVF = CVF)
   }
   
   #prevalence filter
   if(is.null(PF)){
     ps.ws <- ps.ws
   } else {
-    #more ASV table error protection
-    if(as.logical(class(phyloseq::otu_table(ps.ws))[1] == "otu_table") && 
-       as.logical(taxa_are_rows(phyloseq::otu_table(ps.ws)) == TRUE)){
-      otu.tab <- as.matrix(phyloseq::otu_table(ps.ws))
-    } else {
-      otu.tab <- as.matrix(t(phyloseq::otu_table(ps.ws)))
-    }
-    #get prevalence list for each taxon
-    taxa.plist <- apply(X = otu.tab, MARGIN = 1, FUN = function(x){names(x)[which(x!=0)]})
-    #build vector of sample counts per ASV
-    taxa.cvec <- c()
-    for(i in 1:length(taxa.plist)){
-      taxa.cvec[i] <- length(taxa.plist[[i]])
-    }
-    #apply ASV names and filter ASVs below PF
-    names(taxa.cvec) <- names(taxa.plist)
-    prev.count <- phyloseq::nsamples(ps.ws)*PF
-    taxa.cvec.f <- taxa.cvec[which(taxa.cvec > prev.count)]
-    tn.cvec.f <- names(taxa.cvec.f)
-    #filter ps
-    ps.ws <- phyloseq::prune_taxa(tn.cvec.f, ps.ws)
-    
-  }
+    ps.ws <- Pfilter(ps = ps.ws, WSF = WSF, PF = PF)
   
   #create AS filter sample sum vector
   pfv <- phyloseq::sample_sums(ps.ws)
@@ -419,7 +489,7 @@ filter.dataset <- function(ps, controlID=NULL, controlCAT=NULL, controlFACTOR=NU
   }
   
   return(l.return)
-}                                
+} 
                                    
 write.dataset.biom <- function(ps, filepath, fileprefix){
   
@@ -457,8 +527,9 @@ write.dataset <- function(ps, filepath, fileprefix){
   names(f.onames) <- paste0(">", phyloseq::taxa_names(ps))
   
   #generate otu table formatted for biom generation
-  suppressWarnings(otu.tab <- as.matrix(t(phyloseq::otu_table(ps))))
-  cb <- as.matrix(cbind(rownames(otu.tab), otu.tab))
+  asv.tab <- format.ASV.tab(ps)
+  suppressWarnings(asv.tab <- as.matrix(asv.tab)
+  cb <- as.matrix(cbind(rownames(asv.tab), asv.tab))
   rcb <- as.matrix(rbind(colnames(cb), cb))
   rcb[1,1] <- "#ASVID"
   rownames(rcb) <- NULL
@@ -491,7 +562,7 @@ write.dataset <- function(ps, filepath, fileprefix){
   #write output
   #ASV fasta 
   write.table(x = f.onames, file = fa, quote = FALSE, sep = "\n", col.names = FALSE)
-  #otu.tab
+  #asv.tab
   write.table(x = rcb, file = otb, row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
   #tax.tab
   write.table(x = rcbt, file = ttb, row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
